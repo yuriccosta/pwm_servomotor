3c9e3db006d1611504359e562f772967a6f433de
yuriccosta <yuricoutinhoc@gmail.com>
2025-02-10 17:08:51 -0300

    Adicionado alteração para funcionar com led

--- pwm_motor.c
+++ pwm_motor.c
@@ -2,12 +2,12 @@
 #include "pico/stdlib.h" //subconjunto central de bibliotecas do SDK Pico
 #include "hardware/pwm.h" //biblioteca para controlar o hardware de PWM
 
-#define PWM_MOTOR 22  // GPIO utilizada para controle do servo
+#define LED_BLUE 22  // GPIO utilizada para controle do Led Azul
 
 // Função para configurar o PWM na frequência desejada (50 Hz)
 void pwm_setup() {
-    gpio_set_function(PWM_MOTOR, GPIO_FUNC_PWM);         // Configura o pino como saída PWM
-    uint slice = pwm_gpio_to_slice_num(PWM_MOTOR);         // Obtém o slice correspondente
+    gpio_set_function(LED_BLUE, GPIO_FUNC_PWM);         // Configura o pino como saída PWM
+    uint slice = pwm_gpio_to_slice_num(LED_BLUE);         // Obtém o slice correspondente
 
     // Configura o divisor de clock:
     pwm_set_clkdiv(slice, 125.0);
@@ -22,20 +22,20 @@ void pwm_setup() {
 int main() {
     stdio_init_all();  // Inicializa o sistema de I/O
     pwm_setup();       // Configura o PWM
-    uint slice = pwm_gpio_to_slice_num(PWM_MOTOR);
+    uint slice = pwm_gpio_to_slice_num(LED_BLUE);
 
     // 1) Posição para 180° (pulso de 2400 µs)
-    pwm_set_gpio_level(PWM_MOTOR, 2400);
+    pwm_set_gpio_level(LED_BLUE, 2400);
     printf("Posicao: 180 graus\n");
     sleep_ms(5000);  // Aguarda 5 segundos
 
     // 2) Posição para 90° (pulso de 1470 µs)
-    pwm_set_gpio_level(PWM_MOTOR, 1470);
+    pwm_set_gpio_level(LED_BLUE, 1470);
     printf("Posicao: 90 graus\n");
     sleep_ms(5000);  // Aguarda 5 segundos
 
     // 3) Posição para 0° (pulso de 500 µs)
-    pwm_set_gpio_level(PWM_MOTOR, 500);
+    pwm_set_gpio_level(LED_BLUE, 500);
     printf("Posicao: 0 graus\n");
     sleep_ms(5000);  // Aguarda 5 segundos
 
@@ -44,12 +44,12 @@ int main() {
     while (true) {
         // Movimento de 0° para 180°
         for (int level = 500; level <= 2400; level += 5) {
-            pwm_set_gpio_level(PWM_MOTOR, level);
+            pwm_set_gpio_level(LED_BLUE, level);
             sleep_ms(10);  // Atraso de 10 ms entre os incrementos
         }
         // Movimento de 180° para 0°
         for (int level = 2400; level >= 500; level -= 5) {
-            pwm_set_gpio_level(PWM_MOTOR, level);
+            pwm_set_gpio_level(LED_BLUE, level);
             sleep_ms(10);  // Atraso de 10 ms entre os decrementos
         }
     }
